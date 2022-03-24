25712654b4ec0f5c8765100cbed1e86699a5db60
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-24 13:44:00 -0700

    commentary update to code for read line excercise

--- prompt.c
+++ prompt.c
@@ -6,7 +6,8 @@ int main(void)
 	char *buffer;
 	size_t bufsize;  /* figure out why setting this to 32 */
 
-	buffer = malloc(sizeof(char));
+	buffer = malloc(sizeof(char)); /* should i still multiply size of char
+					* by bufsize? */
 	if (buffer == NULL)
 	{
 		return (0);
